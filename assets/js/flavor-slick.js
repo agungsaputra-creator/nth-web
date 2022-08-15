e243b943f0ccd4890986f65f6c6d408bd2f4a417
agung saputra <agung.creativeprod@gmail.com>
2022-08-16 02:03:06 +0700

    update

--- assets/js/flavor-slick.js
+++ assets/js/flavor-slick.js
@@ -2,9 +2,10 @@ $(".flavor-slick").slick({
   // dots: true,
   infinite: true,
   autoplay: true,
-  autoplaySpeed: 3000,
+  autoplaySpeed: 1500,
   slidesToShow: 1,
   slidesToScroll: 1,
+  initialSlide: 0,
   prevArrow: '<span class="prev_arrow"><i class="fa-solid fa-angle-left"></i></span>',
   nextArrow: '<span class="next_arrow"><i class="fa-solid fa-angle-right"></i></span>',
 });
